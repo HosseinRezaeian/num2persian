48623097a15bbf905de5652ebfeb20c7aa6c95e1
h0ssainr <hossainrezaeian@gmail.com>
2022-09-14 00:04:13 +0430

    add 000 in midle

--- test.py
+++ test.py
@@ -1,31 +1,28 @@
-
-
-
 def convert(list):
-      
+
     # Converting integer list to string list
     s = [str(i) for i in list]
-      
+
     # Join list items using join()
     res = int("".join(s))
-      
+
     return(res)
 
 
 zero=False
 
 numtoper1=['','یک','دو','سه','چهار','پنج','شش','هفت','هشت','نه']
-numtoper10=['ده','یازده','دوازده','سیزده','چهارده','پانزده','شانزده','هیفده','هیجده','نوزده',]
+numtoper10=['ده','یازده','دوازده','سیزده','چهارده','پانزده','شانزده','هفده','هجده','نوزده',]
 numtoper90=['بیست','سی','چهل','پنجاه','شصت','هفتاد','هشتاد','نود']
 numtoper100=['صد','دویست','سیصد','چهارصد','پانصد','ششصد','هفتصد','هشتصد','نهصد']
-bignum_name=['هزار','ملیون','ملیارد','بیلیون','بیلیارد','تریلیون','تریلیارد']
+bignum_name=['هزار','میلیون','میلیارد','بیلیون','بیلیارد','تریلیون','تریلیارد']
 
 def lower_t_100(num1):
     global zero
     if num1==0:
         zero=True
         return ''
-    
+
     elif 10>num1>0:
         return numtoper1[num1]
     elif 20>num1>9:
@@ -40,7 +37,7 @@ def lower_t_100(num1):
         elif null_zero[0]>1 and null_zero[1]==0:
             dahgan=numtoper90[null_zero[0]-2]
             return ("{}".format(dahgan))
-    
+
 def biger_t_99(numtoper):
     global zero
     if numtoper==0:
@@ -55,19 +52,19 @@ def biger_t_99(numtoper):
 def finall(num):
     null_zero=list(str(num))
     null_zero=[int(x) for x in null_zero]
-    
+
     if len(null_zero)<3:
         return lower_t_100(num)
     elif null_zero[0]>0 and null_zero[1]==0 and null_zero[2]==0:
         return numtoper100[null_zero[0]-1]
 
     elif len(null_zero)>2:
-        
+
         sadgan=biger_t_99(null_zero[0])
         del null_zero[0]
         low=lower_t_100(convert(null_zero))
         return ("{} و {}".format(sadgan,low))
-    
+
 
 def get_first(numer):
 
@@ -89,26 +86,26 @@ def get_first(numer):
 
 
     n1=[int(x) for x in num1]
-    
+
     if len(n1)>0:
         reverall.append(convert(n1))
-    
+
     ln=len(num)
     li=len(num)-3
-    
 
-    
+
+
     for itn in reverall:
         aller.append(itn)
         # print(aller)
     list_revers=[]
     for t3 in range(0,int(len(num)/3)):
-        
+
         lid=num[li:ln]
         n1=[int(x) for x in lid]
         if len(n1)>0:
             list_revers.append(convert(n1))
-            
+
         ln=ln-3
         li=li-3
     list_revers.reverse()
@@ -120,53 +117,75 @@ def get_first(numer):
     shval=""
     for a in range(0,len(aller)):
         lival.append(finall(aller[a]))
-        
+    global zero
     if len(lival)==1:
         shval="{}".format(lival[0])
-        
+
     else:
         big_all=bignum_name[0:len(lival)-1]
         big_all.reverse()
-        
-        
-        
-        
-        for item in range(0,len(lival)):
-            
-            if zero==True:
-                bgn=big_all[0]
-                shval=' '+lival[item]+' '+bgn
-                return shval
-            elif item<=len(big_all)-1 :
-                bgn=big_all[item]+' و ' 
-            
-            if item==len(lival)-1:
-                bgn=''
 
-            shval+=' '+lival[item]+' '+bgn
+
+
+
+        for item in range(0,len(lival)):
+
+
+            if lival[item]=="":
+                bgn='\b'
+
+
+
+            elif item<=len(big_all)-1 :
+                jamer=0
+                if item+1<=len(aller)-1:
+                    newl=aller[item+1:len(aller)]
+                else:
+                    newl=aller[item:len(aller)]
+
+                print(newl)
+                print(aller)
+                for jam in newl:
+                    jamer+=jam
+                if jamer>0:
+                    bgn=big_all[item]+' و '
+                    print('t')
+                else:
+                    bgn=big_all[item]
+                    print('f')
+
+
+
+            if item==len(lival)-1:
+                bgn='\b'
+
+            shval+=''+lival[item]+' '+bgn
     return shval
 while True:
     s=input('enter num: ')
     print(get_first(int(s)))
 
 
+# ,80000,685466,5464,1000000000,10000000000,100000000001
+
+# def tester():
+#     # list_horf=['ششصد و هشتاد و پنج هزار و چهار صدو شصت و شیش','هشتاد هزار','یک میلیون و یک','یک هزار','چهار هزار و پانصد و شصت و نه','سیصد و شصت و نه','صد و چهل و هفت','صد و پنجاه و سه','صد و یک','نه','یک']
+#     # list_horf.reverse()
+#     # list_number=[1,9,101,153,147,369,4569,1000,1000001,80000,685466]
+#     # for item in range(0,len(list_horf)-1):
+#     #     s=get_first(list_number[item])
+#     #     if s==list_horf[item]:
+#     #         print('true')
+#     #     else:
+#     #         print('------------')
+#     #         print(list_horf[item])
+#     #         print(list_number[item])
+#     #         print(s)
+#     #         print('------------')
+
+#     for i in range(990000,1000005):
+#         print(get_first(i))
 
 
 
-
-        
-
-
-            
-        
-
-
-# while True:
-
-#     iner=input('enter num:')
-#     print(finall(int(iner)))       
-# for i in range(1,1000000000):
-    
-    
-#     print(get_first(int(i)))
-
+# tester()
